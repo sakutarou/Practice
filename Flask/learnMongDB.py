e88dd52bf64a138af5cfe3afbb245969f1925ccf
dfsh <master.am03@g2.nctu.edu.tw>
2021-08-25 16:17:31 +0800

    0825 mongodb to catch data

--- Flask/learnMongDB.py
+++ Flask/learnMongDB.py
@@ -13,3 +13,35 @@
 # JSON 格式友善、簡潔的文件模型、容易水平擴展
 # 在網路上先進入 MongoDB 的網站，建立好 DataBase 與連結入口
 # pip install pymongo[srv] 
+# 記得有些電腦要處理 ssl 的問題
+
+# 資料庫結構
+# Mongo DB 的結構：資料庫 DataBase、集合Collection、文件Documents
+
+# 新增 JSON 格式的資料
+# result = 集合.insert_one(文件資料)
+# result.inserted_id 取得新增資料的編號
+# 新增多筆資料
+# result = collection.insert_many([{data1},{data2},{data3}])
+# result.inserted_ids
+
+# 取得文件資料
+# 集合.find_one() # 取得集合中的第一筆資料
+# 根據編號 ObjectId 物件取得文件資料，必須從 bson.objectId 封包載入
+# 集合.find_one(文件編號)
+# e.g.
+# from bson.objectid import ObjectId
+# collection = db.website
+# data = collection.find_one(
+#     ObjectId("....")
+# )
+# print(data["name"]) 顯示該資料中的 name 欄位內容
+# print(data["_id"])
+# 取得所有文件資料
+# 集合.find()
+# collection = db.website
+# cursor = collection.find()
+# print(cursor) # Cursor 物件
+# 使用 for 迴圈逐一取得文件
+# for doc in cursor:
+#   print(doc)
--- Flask/mongoPractice.py
+++ Flask/mongoPractice.py
@@ -1,14 +1,49 @@
 # 載入 ptmongo 套件
 import pymongo
-# 連線到雲端資料庫
-client = pymongo.MongoClient("mongodb+srv://root:root@mycluster.xgf87.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
-# 把資料放進資料庫中
-db = client.test # 選擇操作 test 的資料庫
-collection = db.users # 選擇操作 users 集合
-# 把資料新增到集合中
-collection.insert_one({
-    "name":"john",
-    "gender":"男"
-})
-print("資料新增成功")
+import ssl
+from bson.objectid import ObjectId
 
+# 連線到雲端資料庫
+client = pymongo.MongoClient("mongodb+srv://root:root123@mycluster.xgf87.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",ssl_cert_reqs = ssl.CERT_NONE)
+# 把資料放進資料庫中
+db = client.website # 選擇操作 website 的資料庫
+collection = db.members # 選擇操作 members 集合
+
+# 把一筆資料新增到集合中
+# result = collection.insert_one({
+#     "name":"john",
+#     "email":"test@test.com",
+#     "password":"test",
+#     "level":1
+# })
+
+# 把多筆資料新增到集合中
+# result = collection.insert_many([{
+#     "name":"lary",
+#     "email":"a@a.com",
+#     "password":"a",
+#     "level":2
+# },{
+#     "name":"mary",
+#     "email":"b@b.com",
+#     "password":"b",
+#     "level":3
+# }])
+
+# 取得集合中的第一筆文件資料
+# data = collection.find_one()
+# # 取得集合中的某特定文件資料
+# data = collection.find_one(ObjectId("6125cd276c1453fee8f20337"))
+
+# print(data["_id"]) # 印出該內容中的id
+# print(data["email"])
+
+# 一次取得多筆文件的資料
+cursor = collection.find() # 這是一個讀寫頭
+print(cursor)
+for doc in cursor:
+    print(doc["name"])
+
+# 印出該文件的 id
+# print(result.inserted_ids)
+# print("資料新增成功")
