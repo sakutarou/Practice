c85d2b845b2d8e2d393c2180deac75b5a05684a7
sakutarou <>
2021-09-14 01:06:10 +0800

    connect to mongodb

--- Mongodb/mongodb.js
+++ Mongodb/mongodb.js
@@ -1,9 +1,8 @@
-var MongoClient = require("mongodb").MongoClient; //呼叫並操作 MongoDB
-var url = "mongodb+srv://root:root@mycluster.xgf87.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"; //建立一個叫做 mydb 的資料庫
-
-console.log("hello");
-MongoClient.connect(url,function(err,db){
-    if(err) throw err;
-    console.log("DataBase created!");
-    db.close();
+const { MongoClient } = require('mongodb'); //呼叫並操作 MongoDB;
+const uri = "mongodb+srv://root:root@mycluster.xgf87.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";//建立一個叫做 myFirstDatabase 的資料庫
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
 });
